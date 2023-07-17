fbc341a78bc76ad8fec25de0c1ffed0e27f644af
phuong.nguyen <56102858+giaotrinh147@users.noreply.github.com>
2023-07-17 17:16:22 +0700

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 # *nix only
-export RG="reddog-png_1"
+export RG="reddog-png_2"
 export LOCATION="eastus2"
 export SUB_ID="c4d65733-80ea-4652-917f-6fd98aa99aa4"
 
@@ -9,7 +9,6 @@ az account set --subscription $SUB_ID
 
 # Create resource group
 az group create -n $RG -l $LOCATION
-
 # Deploy all infrastructure and reddog apps
 az deployment group create -n reddog -g $RG -f ./deploy/bicep/main.bicep
 
